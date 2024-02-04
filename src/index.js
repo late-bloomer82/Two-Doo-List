866311cb2b6277d60b2486a2d3fc7f405804f72d
late-bloomer82 <nateriver72@outlook.com>
2024-02-03 23:00:58 -0500

    import statements for imgs and main page DOM function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import webIcon from './dom';
+import homeIcon from './dom';
+import todayIcon from './dom';
+import weekIcon from './dom';
+import plusIcon from './images/plus.svg'
 import './styles.css';
+import { mainPageContent } from './dom';
+
+document.addEventListener('DOMContentLoaded', mainPageContent);
