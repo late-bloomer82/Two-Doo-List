6924e5ea169b708c2f4ae13f071e91ba19848631
late-bloomer82 <nateriver72@outlook.com>
2024-02-08 18:18:02 -0500

    added a few imports statments for my assets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import homeIcon from './dom';
 import todayIcon from './dom';
 import weekIcon from './dom';
 import plusIcon from './images/plus.svg'
+import priorityIconGreen from './dom';
+import priorityIconYellow from './dom';
+import priorityIconRed from './dom';
+import priorityIconDarkRed from './dom';
+import checkIcon from './dom';
+import dotsIcon from './dom';
 import './styles.css';
 import { mainPageContent } from './dom';
 
