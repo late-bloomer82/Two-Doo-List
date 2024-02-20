5bdbb0108e91600b2f024126a653355cc2ace328
late-bloomer82 <nateriver72@outlook.com>
2024-02-20 18:17:10 -0500

    just added a comment

---
+++ src/displayProject.js
@@ -0,0 +1,25 @@
+import {projects} from "./dom";
+import {addContent ,taskContainer} from "./toDo";
+
+export {displayProject};
+
+
+function displayProject(projectName) {
+    
+    //Clear Content
+    taskContainer.innerHTML = '';
+
+    for (let i = 0; i < projects[projectName].length; i++) {
+        const taskObject = projects[projectName][i];
+        const taskTitle = taskObject.title;
+        const taskDescription = taskObject.description;
+        const taskDueDate = taskObject.dueDate;
+        const taskPriority = taskObject.priority;
+        addContent(taskTitle, taskPriority, projectName, taskDescription, taskDueDate);
+
+       
+        
+    }
+     
+   
+}
