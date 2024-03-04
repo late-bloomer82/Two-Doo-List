a21960334e2c6819aa1565d8571eaed9b6af1470
late-bloomer82 <nateriver72@outlook.com>
2024-03-03 19:27:25 -0500

    fixed the issue of duplicate img outputs by webpack and exported all the img files for this indexjs file to be the central file for img imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import webIcon from './dom';
-import homeIcon from './dom';
-import todayIcon from './dom';
-import weekIcon from './dom';
-import plusIcon from './images/plus.svg'
-import priorityIconGreen from './dom';
-import priorityIconYellow from './dom';
-import priorityIconRed from './dom';
-import priorityIconDarkRed from './dom';
-import checkIcon from './dom';
-import dotsIcon from './dom';
+import webIcon from './images/webIcon.jpg';
+import homeIcon from './images/home.svg';
+import todayIcon from './images/calendar-today.svg';
+import weekIcon from './images/calendar-week.svg';
+import priorityIconGreen from './images/priorityIconGreen.svg';
+import priorityIconYellow from './images/priorityIconYellow.svg';
+import priorityIconRed from './images/priorityIconRed.svg';
+import priorityIconDarkRed from './images/priorityIconDarkRed.svg';
+import checkIcon from './images/check-circle.svg';
+import dotsIcon from './images/dots-horizontal.svg';
+import completedCheck from './images/check-completed.svg'
+import closeIcon from './images/close.svg';
 import './styles.css';
 import { mainPageContent } from './dom';
+
+export{ closeIcon,webIcon,homeIcon,todayIcon,weekIcon,priorityIconGreen,priorityIconYellow,priorityIconRed, priorityIconDarkRed,checkIcon,dotsIcon,completedCheck}
 
 document.addEventListener('DOMContentLoaded', mainPageContent);
