2f45f39468318e0c96cfedcd7c2de069fdcd9902
late-bloomer82 <nateriver72@outlook.com>
2024-03-03 19:15:31 -0500

    cleanup

--- src/displayProject.js
+++ src/displayProject.js
@@ -1,26 +1,36 @@
 import {projects} from "./dom";
-import {addContent ,taskContainer} from "./toDo";
+import {addContent,noTasks,taskContainer,checkifProjectIsEmpty} from "./js_modules";
+
 
 export {displayProject};
 
 
 function displayProject(projectName) {
     
-    //Clear Content
-    taskContainer.innerHTML = '';
-
+     //Clear Content
+     taskContainer.innerHTML = '';
+     if(checkifProjectIsEmpty(projectName)){
+        noTasks.style.display ='flex';
+     }
+   
+   
+    else{
     for (let i = 0; i < projects[projectName].length; i++) {
         const taskObject = projects[projectName][i];
         const taskTitle = taskObject.title;
         const taskDescription = taskObject.description;
         const taskDueDate = taskObject.dueDate;
         const taskPriority = taskObject.priority;
-        addContent(taskTitle, taskPriority, projectName, taskDescription, taskDueDate);
-
+        const id = taskObject.id
+        
+        addContent(taskTitle, taskPriority, projectName, taskDescription, taskDueDate,id);
+        
+        
        
         
     }
-     
+}
+    
    
 }
 
