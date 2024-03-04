088c0a377d78969325ebebc1ccb197508b91337b
late-bloomer82 <nateriver72@outlook.com>
2024-03-03 19:15:08 -0500

    added a function that modifies calendar arrays based on user editing a task's due date

--- src/compareDates.js
+++ src/compareDates.js
@@ -6,7 +6,7 @@ import { homeTasks } from './home';
 import { todayTasks } from './today';
 import { weekTasks } from './week';
 
-export{addTaskToRespectiveArray};
+export{addTaskToRespectiveArray, editTaskArray};
 
 
 
@@ -41,11 +41,8 @@ function addTaskToRespectiveArray(){
     const getWeek = getWeekInterval()
 
     //Home
-    if(1===1){
-        homeTasks.push(toDo)
-        
-    }
-    
+    homeTasks.push(toDo)
+
     //Today
     if(toDo.dueDate == currentStringDate){
         todayTasks.push(toDo)
@@ -59,7 +56,55 @@ function addTaskToRespectiveArray(){
  
  }
 
+ function editTaskArray(task, taskId){
+    const currentStringDate = convertDateToString()
+    
+    const currentDate = new Date();
+        
+    const SevenDaysAfterCurrentDate = add(currentDate,{days:7});
+        
+    const taskDueDate = new Date(task.dueDate)
+      
+    let Week = isWithinInterval(taskDueDate, {start: currentDate, end: SevenDaysAfterCurrentDate });
+   
+    //Remove from current calendar arrays
+    removeTaskFromCalendarArray(taskId)
+
+    
+    //Add to updated calendar arrays
+    homeTasks.push(task)
+
+    if(task.dueDate == currentStringDate){
+        todayTasks.push(task)
+    }
+    
+    if(Week){
+       
+       weekTasks.push(task);
+    }
+ }
 
 
 
+
+ function removeTaskFromCalendarArray(taskId){
+    
+    const homeIndexToRemove = homeTasks.findIndex(task => task.id === taskId);
+
+    const todayIndexToRemove = todayTasks.findIndex(task => task.id === taskId);
+
+    const weekIndexToRemove = weekTasks.findIndex(task => task.id === taskId);
+
+    if (homeIndexToRemove !== -1) {
+        homeTasks.splice(homeIndexToRemove, 1);
+    }
+
+    if (todayIndexToRemove !== -1) {
+        todayTasks.splice(todayIndexToRemove, 1);
+    }
+
+    if (weekIndexToRemove !== -1) {
+        weekTasks.splice(weekIndexToRemove, 1);
+    }
+}
  
